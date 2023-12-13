75df30e474504054337ef0157c49e4e69f6f6452
pilboy97 <76037604+pilboy97@users.noreply.github.com>
2023-12-13 09:56:38 +0900

    Update dynamic_array.c

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,13 +1,16 @@
 #include "dynamic_array.h"
 
 void __append(DynamicArray* arr, void* elem) {
+	bool renewal = false;
 	while(arr->len + 1 > arr->cap) { // 만약 배열이 가득 찼다면
 		if(arr->cap == 0) { // 만약 배열의 길이가 0이라면
 			arr->cap = 1;	// 배열의 길이를 1로 설정
-		} 
-		else {
+		} else {
 			arr->cap *= 2;	// 배열의 길이를 2배로 바꿈
 		}
+		renewal = true;
+	}
+	if(renewal) {
 		void* neo = malloc(arr->elemSize * arr->cap); // 새 배열을 할당함
 		void* old = arr->ptr;	// 이전 배열 포인터
 
@@ -16,7 +19,6 @@ void __append(DynamicArray* arr, void* elem) {
 		arr->ptr = neo; // 배열 포인터 갱신
 		free(old);	// 기존 배열 삭제
 	}
-
 	memcpy((char*) arr->ptr + arr->len * arr->elemSize, elem, arr->elemSize); // 배열의 마지막에 새 원소 추가
 	arr->len += 1;	// 길이 증가
 }
@@ -29,4 +31,8 @@ void da_fit(DynamicArray* arr) { // 동적 배열 길이에 배열 사이즈를 
 
 	arr->ptr = neo; // 배열 포인터 갱신
 	free(old);	// 기존 배열 삭제
+}
+
+void da_free(DynamicArray* arr) {
+	free(arr->ptr);
 }
